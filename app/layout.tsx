03d0f81aa0730133dcd45798559ee0a33d4c830c
AllaArestakesyan <alla.arestakesyan@gmail.com>
2024-11-04 14:32:07 +0400

    ...

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,7 @@ export default function RootLayout({ children }: Props) {
   return (
     <html lang="en">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Dosis&family=Inconsolata:wght@300&family=Istok+Web:ital,wght@0,400;0,700;1,400;1,700&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Syne:wght@400..800&display=swap" rel="stylesheet" />
